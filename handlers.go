4b07ff24d18a37e3c4e27fde9288198d93449c49
Lander Brandt <landerbrandt@gmail.com>
2015-05-07 23:24:23 -0700

    More tidying

--- handlers.go
+++ handlers.go
@@ -39,3 +39,121 @@ func readRequestData(r *http.Request) (returnData map[string]interface{}, err er
 
 	return
 }
+
+func setupRoutes(router *httprouter.Router) {
+	router.Handle("GET", "/db", handleDb)
+
+	// set up our routes
+	for key, value := range data {
+		// Shadow these variables. If this isn't done, then the closures below will see
+		// `value` and `key` as whatever they were in the last(?) iteration of the above for loop
+		value := value
+		key := key
+
+		router.GET(fmt.Sprintf("/%s", key), func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+			genericJsonResponse(w, r, value)
+		})
+
+		for _, method := range []string{"GET", "PATCH", "PUT", "DELETE"} {
+			method := method
+			router.Handle(method, fmt.Sprintf("/%s/:id", key), func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+				rows, ok := value.([]interface{})
+				if !ok {
+					logger.Error("unknown type")
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				idParam, _ := strconv.ParseFloat(ps.ByName("id"), 64)
+				for i, row := range rows {
+					rowMap, _ := row.(map[string]interface{})
+
+					// Found the item
+					if id, ok := rowMap["id"]; ok && id.(float64) == idParam {
+
+						// The method type determines how we respond
+						switch method {
+						case "GET":
+							genericJsonResponse(w, r, row)
+							return
+						case "PATCH":
+							updatedData, err := readRequestData(r)
+							if err != nil {
+								w.WriteHeader(http.StatusBadRequest)
+								return
+							}
+
+							dataMutex.Lock()
+							for key, value := range updatedData {
+								rowMap[key] = value
+							}
+
+							dirty = true
+
+							// since value is a shadow copy, we need to update it as it's now stale
+							value = data[key]
+
+							dataMutex.Unlock()
+
+							return
+						case "PUT":
+							updatedData, err := readRequestData(r)
+							if err != nil {
+								w.WriteHeader(http.StatusBadRequest)
+								return
+							}
+
+							dataMutex.Lock()
+							for key, _ := range rowMap {
+								rowMap[key] = nil
+							}
+
+							for key, value := range updatedData {
+								rowMap[key] = value
+							}
+
+							dirty = true
+
+							// since value is a shadow copy, we need to update it as it's now stale
+							value = data[key]
+
+							dataMutex.Unlock()
+
+							w.WriteHeader(http.StatusOK)
+
+							return
+						case "DELETE":
+							dataMutex.Lock()
+							data[key] = append(rows[:i], rows[i+1:]...)
+							dirty = true
+
+							// since value is a shadow copy, we need to update it as it's now stale
+							value = data[key]
+
+							dataMutex.Unlock()
+
+							w.WriteHeader(http.StatusOK)
+							return
+						}
+					}
+				}
+
+				if method == "PUT" {
+					newData, err := readRequestData(r)
+					if err != nil {
+						w.WriteHeader(http.StatusBadRequest)
+						return
+					}
+
+					dataMutex.Lock()
+					dirty = true
+					data[key] = append(rows, newData)
+					dataMutex.Unlock()
+
+					w.WriteHeader(http.StatusCreated)
+				}
+				w.WriteHeader(http.StatusNotFound)
+			})
+		}
+	}
+}
